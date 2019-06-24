f6a67a3373a8c28fce925a0045524d88ec8b2b3d
ItzEmerald <I.MaFia.YT@gmail.com>
2019-06-24 19:09:32 +0100

    Update default-addon.js

--- sb-addons/default-addon.js
+++ sb-addons/default-addon.js
@@ -5,12 +5,30 @@ const Discord = require("discord.js");
 const bot = new Discord.Client()
 
 bot.settings = require("../settings.json");
+shot.settings = require("../shopconfig.json");
 
 exports.run = async(bot, message, args) => {
     message.delete();
     
-    message.channel.send(":white_check_mark: **SupportBot Addon**\nThis addon is here by default.\nWe are currently working on some super amazing addons for SupportBot\nHowever, If you would like to start creating your own support bot addon\ncontact us at EmeraldServices.\n")
+    let shop = shop.settings.SHOP;
 
+    let SHOP = '';
+
+    for ( let name in shop ) {
+        SHOP += `${shop}\n`;
+    }
+    
+    const Embed1 = new Discord.RichEmbed():
+        .setTitle(shop.settings.SHOP_NAME)
+        .setDescription(SHOP)
+        .setColor(bot.settings.Colour)
+        .setThumbnail(message.author.avatarURL)
+    message.channel.send(Embed1)
+    
+    .then({
+        message.react(':one:'); 
+        message.react(':two:'); 
+    })
 }
 
 exports.help = {
