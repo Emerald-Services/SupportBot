197bda14b8460e44356202d4f6f24e7ff9c5a829
ItzEmerald <I.MaFia.YT@gmail.com>
2019-11-29 22:34:57 +0000

    Update rename.js

--- commands/rename.js
+++ commands/rename.js
@@ -26,7 +26,7 @@ exports.run = (bot, message, args) => {
 		.setColor(bot.settings.colour) 
 	if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send({embed: outsideticket});
 	const renameto = args.join(" ")
-	message.channel.setName(`ticket-${renameto}-${message.author.username}`)
+	message.channel.setName(`ticket-${renameto}`)
 	
     // Renamed Ticket Logistic   
 	const logEmbed = new Discord.RichEmbed()
