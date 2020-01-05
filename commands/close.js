3336824df7cd0f38a90c2371aa3a669542b01687
ItzEmerald <I.MaFia.YT@gmail.com>
2020-01-05 15:22:50 +0000

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -72,8 +72,8 @@ exports.run = ( bot, message, args ) => {
                                 }
                             } );
 
-                            message.author.send( `A transcript has been generated for the ticket you closed.` );
-                            message.author.send( new Discord.Attachment( Buffer.from( txt ), `${name}.txt` ) );
+                           // message.author.send( `A transcript has been generated for the ticket you closed.` );
+                           // message.author.send( new Discord.Attachment( Buffer.from( txt ), `${name}.txt` ) );
 
                             logChannel.send( logEmbed );
                             logChannel.send( new Discord.Attachment( Buffer.from( txt ), `${name}.txt` ) );
