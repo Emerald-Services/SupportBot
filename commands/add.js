67b291eb09ac292734a89087eb5e8765ee1bc6fa
ItzEmerald <I.MaFia.YT@gmail.com>
2020-01-25 13:30:13 +0000

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -7,40 +7,40 @@ const bot = new Discord.Client()
 
 bot.settings = require("../settings.json");
 
-module.exports.run = async (bot, message, args) => {
-    message.delete()
+module.exports.run = (bot, message, args) => {
+    message.delete();
     
-    console.log(`\u001b[31;1m`, `${message.author.tag}`, `\u001b[32;1m`, `has executed`, `\u001b[31;1m`, `${bot.settings.prefix}${bot.settings.Add_Command}`)
+    console.log(`\u001b[31;1m`, `${message.author.tag}`, `\u001b[32;1m`, `has executed`, `\u001b[31;1m`, `${bot.settings.prefix}${bot.settings.Add_Command}`);
     
-    let staffGroup = message.guild.roles.find(staffRole => staffRole.name === bot.settings.staff)
+    let staffGroup = message.guild.roles.find(staffRole => staffRole.name === bot.settings.staff);
 
     const rolemissing = new Discord.RichEmbed()
         .setDescription(`:x: Looks like this server doesn't have the role **${bot.settings.staff}**`)
-        .setColor(bot.settings.colour) 
+        .setColor(bot.settings.colour);
     if (!staffGroup) return message.reply({embed: rolemissing});
 
     const donothaverole = new Discord.RichEmbed()
         .setDescription(`:x: Sorry! You cannot use this command with the role **${bot.settings.staff}**`)
-        .setColor(bot.settings.colour) 
+        .setColor(bot.settings.colour); 
     if (!message.member.roles.has(staffGroup.id)) return message.reply({embed: donothaverole});
 
     const outsideticket = new Discord.RichEmbed()
         .setDescription(`:x: Cannot use this command becase you are outside a ticket channel.`)
-        .setColor(bot.settings.colour) 
+        .setColor(bot.settings.colour); 
     if (!message.channel.name.startsWith(`${bot.settings.Ticket_Channel_Name}-`)) return message.channel.send({embed: outsideticket});
 
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
     const cantfinduser = new Discord.RichEmbed()
         .setDescription(`:x: Hmm! Does that user exist? I cannot find the user.`)
-        .setColor(bot.settings.colour) 
+        .setColor(bot.settings.colour); 
     if(!rUser) return message.channel.send({embed: cantfinduser});
 
     const channel = message.guild.channels.find(channel => channel.name === message.channel.name);
 
     const cantfindchannel = new Discord.RichEmbed()
         .setDescription(`:x: Hmm! Does that ticket exist? I cannot find the ticket channel.`)
-        .setColor(bot.settings.colour) 
+        .setColor(bot.settings.colour); 
     
     if(!channel) return message.channel.send({embed: cantfindchannel});
         message.delete().catch(O_o=>{});
@@ -52,12 +52,9 @@ module.exports.run = async (bot, message, args) => {
         .setDescription(`👍 ${rUser} has been added to this ticket`)
         .setTimestamp();
     message.channel.send({embed: useradded});
-
-    let CommandLog = message.guild.channels.find(LogsChannel => LogsChannel === `${bot.settings.Command_Log_Channel}`);
-    if(!CommandLog) return message.channel.send(`:x: Error! Could not find the logs channel. **${bot.settings.Command_Log_Channel}**\nThis can be changed via ``settings.json```);
-
-}
+    
+};
 
 exports.help = {
     name: bot.settings.Add_Command,
-  }
+};
