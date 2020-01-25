d1809587a89ecacb2f96f59d09f26493cd9c5c76
ItzEmerald <I.MaFia.YT@gmail.com>
2020-01-25 13:20:44 +0000

    Updated add.js
    Cleaned and updated the code within the file alongside the console.log for executing the command.

--- commands/add.js
+++ commands/add.js
@@ -1,5 +1,6 @@
 // SupportBot
-// Command: Add User
+// Created by © 2020 Emerald Services
+// Command: Add
 
 const Discord = require("discord.js");
 const bot = new Discord.Client()
@@ -8,50 +9,50 @@ bot.settings = require("../settings.json");
 
 module.exports.run = async (bot, message, args) => {
     message.delete()
+    
+    console.log(`\u001b[31;1m`, `${message.author.tag}`, `\u001b[32;1m`, `has executed`, `\u001b[31;1m`, `${bot.settings.prefix}${bot.settings.Add_Command}`)
+    
+    let staffGroup = message.guild.roles.find(staffRole => staffRole.name === bot.settings.staff)
 
-console.log(`\x1b[36m`, `${message.author} has executed ${bot.settings.prefix}${bot.settings.Add_Command}`)
+    const rolemissing = new Discord.RichEmbed()
+        .setDescription(`:x: Looks like this server doesn't have the role **${bot.settings.staff}**`)
+        .setColor(bot.settings.colour) 
+    if (!staffGroup) return message.reply({embed: rolemissing});
 
-let staffGroup = message.guild.roles.find(staffRole => staffRole.name === bot.settings.staff)
+    const donothaverole = new Discord.RichEmbed()
+        .setDescription(`:x: Sorry! You cannot use this command with the role **${bot.settings.staff}**`)
+        .setColor(bot.settings.colour) 
+    if (!message.member.roles.has(staffGroup.id)) return message.reply({embed: donothaverole});
 
-const rolemissing = new Discord.RichEmbed()
-    .setDescription(`:x: Looks like this server doesn't have the role **${bot.settings.staff}**`)
-    .setColor(bot.settings.colour) 
-if (!staffGroup) return message.reply({embed: rolemissing});
+    const outsideticket = new Discord.RichEmbed()
+        .setDescription(`:x: Cannot use this command becase you are outside a ticket channel.`)
+        .setColor(bot.settings.colour) 
+    if (!message.channel.name.startsWith(`${bot.settings.Ticket_Channel_Name}-`)) return message.channel.send({embed: outsideticket});
 
-const donothaverole = new Discord.RichEmbed()
-    .setDescription(`:x: Sorry! You cannot use this command with the role **${bot.settings.staff}**`)
-    .setColor(bot.settings.colour) 
-if (!message.member.roles.has(staffGroup.id)) return message.reply({embed: donothaverole});
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-const outsideticket = new Discord.RichEmbed()
-    .setDescription(`:x: Cannot use this command becase you are outside a ticket channel.`)
-    .setColor(bot.settings.colour) 
-if (!message.channel.name.startsWith(`${bot.settings.Ticket_Channel_Name}-`)) return message.channel.send({embed: outsideticket});
+    const cantfinduser = new Discord.RichEmbed()
+        .setDescription(`:x: Hmm! Does that user exist? I cannot find the user.`)
+        .setColor(bot.settings.colour) 
+    if(!rUser) return message.channel.send({embed: cantfinduser});
 
-let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    const channel = message.guild.channels.find(channel => channel.name === message.channel.name);
 
-const cantfinduser = new Discord.RichEmbed()
-    .setDescription(`:x: Hmm! Does that user exist? I cannot find the user.`)
-    .setColor(bot.settings.colour) 
-if(!rUser) return message.channel.send({embed: cantfinduser});
+    const cantfindchannel = new Discord.RichEmbed()
+        .setDescription(`:x: Hmm! Does that ticket exist? I cannot find the ticket channel.`)
+        .setColor(bot.settings.colour) 
+    if(!channel) return message.channel.send({embed: cantfindchannel});
+        message.delete().catch(O_o=>{});
+        message.channel.overwritePermissions(rUser, { READ_MESSAGES: true, SEND_MESSAGES: true });
 
-const channel = message.guild.channels.find(channel => channel.name === message.channel.name);
+    const useradded = new Discord.RichEmbed()
+        .setColor(bot.settings.colour) 
+        .setDescription(`:white_check_mark: Successfully Added ${rUser} to the ticket.`)
+        .setTimestamp();
 
-const cantfindchannel = new Discord.RichEmbed()
-    .setDescription(`:x: Hmm! Does that ticket exist? I cannot find the ticket channel.`)
-    .setColor(bot.settings.colour) 
-if(!channel) return message.channel.send({embed: cantfindchannel});
-message.delete().catch(O_o=>{});
-
-message.channel.overwritePermissions(rUser, { READ_MESSAGES: true, SEND_MESSAGES: true });
-
-const useradded = new Discord.RichEmbed()
-    .setColor(bot.settings.colour) 
-    .setDescription(`:white_check_mark: Successfully Added ${rUser} to the ticket.`)
-    .setTimestamp();
-
-message.channel.send({embed: useradded});
-console.log(`\x1b[36m`, `${message.author} has successfully added ${rUser} to ${message.channel}`)
+    message.channel.send({embed: useradded});
+    
+    console.log(`\x1b[36m`, `${message.author.tag} has successfully added ${rUser} to ${message.channel}`)
 
     const CMDLog = new Discord.RichEmbed()
         .setTitle(bot.settings.Commands_Log_Title)
