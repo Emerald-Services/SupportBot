b72fcf39da558d299f3d0f55998e65378dff7b95
ItzEmerald <I.MaFia.YT@gmail.com>
2020-01-26 23:53:53 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -15,12 +15,12 @@ exports.run = (bot, message, args) => {
     console.log(`\u001b[33m`, `[${supportbot.Bot_Name}] > `, `\u001b[31;1m`, `${message.author.tag}`, `\u001b[32;1m`, `has executed`, `\u001b[31;1m`, `${supportbot.Prefix}${supportbot.Help_Command}`);
 
     let userCommands = "";
-        userCommands += `**${supportbot.Prefix}${supportbot.Link_Command}**: Get some important links!\n`;
-        userCommands += `**${supportbot.Prefix}${supportbot.Ping_Command}**: Check the bots ping. Pong!\n`;
 	    userCommands += `**${supportbot.Prefix}${supportbot.Ticket_Command} <reason>**: Create a support ticket\n`;
         userCommands += `**${supportbot.Prefix}${supportbot.Close_Command}**: Close your support ticket\n`;
         userCommands += `**${supportbot.Prefix}${supportbot.Suggest_Command} <suggestion>**: Create a server suggestion\n`;
         userCommands += `**${supportbot.Prefix}${supportbot.Report_Command} <user> <reason>**: Report a valid user\n`;
+        userCommands += `**${supportbot.Prefix}${supportbot.Link_Command}**: Get some important links!\n`;
+        userCommands += `**${supportbot.Prefix}${supportbot.Ping_Command}**: Check the bots ping. Pong!\n`;
 	
     let supportCommands = "";
 	    supportCommands += `**${supportbot.Prefix}${supportbot.Add_Command} <user>**: Adds a user to a ticket\n`;
@@ -37,9 +37,9 @@ exports.run = (bot, message, args) => {
 
     const embed = new Discord.RichEmbed()
         .setTitle(supportbot.Bot_Name)
-        .addField(":ticket: Support Commands", userCommands)
-        .addField(":pushpin: Support Commands", supportCommands, true)
-        .addField(":pushpin: Staff Commands", staffCommands, true)
+        .addField("👨 User Commands", userCommands)
+        .addField(":pushpin: Support Commands", supportCommands)
+        .addField("🔑 Staff Commands", staffCommands)
         .setColor(supportbot.EmbedColour)
         .setFooter(supportbot.EmbedFooter, message.author.displayAvatarURL);
 	message.channel.send(embed);
