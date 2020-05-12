73c89565f0eb50415c27a26bc46b0f332e4bfc0a
ItzEmerald <I.MaFia.YT@gmail.com>
2020-05-12 23:32:01 +0100

    Update help.js

--- commands/System/help.js
+++ commands/System/help.js
@@ -1,18 +1,4 @@
 const Command = require('../../structures/Command');
 const { MessageEmbed } = require('discord.js');
 
-module.exports = class Help extends Command {
-    constructor(client) {
-        super(client, {
-            name: 'help',
-            aliases: ['h', '?'],
-            description: `Get a list of commands that I offer!`,
-            usage: '[Command?]'
-        });
-    }
-
-    async exec(message, args) {
-       const help = new MessageEmbed()
-       .setAuthor(`Prefix = \`${message.guild.settings.prefix}\``)
-    }
-}
+module.exports = class Help extends Command {}
