79ce1dffa56861890012231f00fe9021d1e6ae02
ItzEmerald <I.MaFia.YT@gmail.com>
2020-06-07 14:45:01 +0100

    Update newticket.js

--- commands/newticket.js
+++ commands/newticket.js
@@ -6,3 +6,24 @@ const fs = require("fs")
 const yaml = require('js-yaml');
 
 const supportbot = yaml.load(fs.readFileSync('./supportbot-config.yml', 'utf8'));
+
+module.exports = {
+    name: supportbot.NewTicket,
+
+    execute(message, args) {
+      const author = sequelize.define('TicketAuthor', {
+        name: {
+          type: Sequelize.STRING,
+          unique: true,
+        },
+        
+        description: Sequelize.TEXT,
+	      username: Sequelize.STRING,
+	      usage_count: {
+          type: Sequelize.INTEGER,
+		      defaultValue: 0,
+		      allowNull: false,
+        }
+    });
+  }
+};
