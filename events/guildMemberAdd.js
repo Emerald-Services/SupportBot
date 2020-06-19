4ae6dc3a69b811b217b600daad6128507ba02bb6
ItzEmerald <I.MaFia.YT@gmail.com>
2020-06-20 00:18:17 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -9,5 +9,15 @@ const supportbot = yaml.load(fs.readFileSync('./supportbot-config.yml', 'utf8'))
 
   
 module.exports = async (bot) => {
+  if (supportbot.SystemMessages === "true") {
     const SystemChannel = message.guild.channels.cache.find(channel => channel.name === supportbot.SystemMessage_Channel)
+    
+    if (!SystemChannel) return;
+    
+    if (supportbot.SystemMessage_Embed === "true") {
+      const GuildAddMember = new Discord.MessageEmbed()
+        
+    }
+    
+  }
 };
