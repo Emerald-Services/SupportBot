dcc00d8d1fc7a93b8b75ae442528c92d751320ec
ItzEmerald <I.MaFia.YT@gmail.com>
2020-07-11 13:17:57 +0100

    Update

--- commands/announce.js
+++ commands/announce.js
@@ -16,19 +16,19 @@ module.exports = {
         let locateChannel = message.guild.channels.cache.find(AnnouncementChannel => AnnouncementChannel.name === supportbot.AnnouncementChannel);
 
         const errornochannel = new Discord.MessageEmbed()
-            .setTitle("SupportBot Error!")
-            .setDescription(`:x: **Error!** Channel not Found, This command cannot be executed proberbly as their is no channel within this server.\nThis is configurable via **supportbot-config.yml**\n\nChannel Required: \`${supportbot.AnnouncementChannel}\`\n\nError Code: \`SB-03\``)
+            .setTitle("Invalid Channel")
+            .setDescription(`:x: **Error!** Channel not Found, This command cannot be executed proberbly as their is no channel within this server.\nThis is configurable via **supportbot-config.yml**\n\nChannel Required: \`${supportbot.AnnouncementChannel}`)
             .setColor(supportbot.ErrorColour);
 
         if(!locateChannel) return message.channel.send({ embed: errornochannel });
 
         let serverAdmins = message.guild.roles.cache.find(adminRole => adminRole.name === supportbot.Admin);
 
-        const rolerequired = new Discord.MessageEmbed()
-            .setTitle("SupportBot Error!")
-            .setDescription(`:x: **Error!** Incorrect Permissions, You cannot execute this command as you do not have the required role.\n\nRole Required: \`${supportbot.Admin}\`\n\nError Code: \`SB-02\``)
-            .setColor(supportbot.ErrorColour); 
-        if (!message.member.roles.cache.has(serverAdmins.id)) return message.reply({embed: rolerequired});
+        const NoPerms = new Discord.MessageEmbed()
+            .setTitle("Invalid Permissions!")
+            .setDescription(`:warning: **Err!** You do not have the correct permissions to use this command.\n\nRole Required: \`${supportbot.serverAdmins}\``)
+            .setColor(supportbot.WarningColour)
+        message.channel.send({ embed: NoPerms });
 
 
             const embed = new Discord.MessageEmbed()
--- commands/removeUser.js
+++ commands/removeUser.js
@@ -1,5 +1,5 @@
 // SupportBot 6.0, Created by Emerald Services
-// Add User
+// Remove User
 
 const Discord = require("discord.js");
 const fs = require("fs");
@@ -8,16 +8,15 @@ const yaml = require('js-yaml');
 const supportbot = yaml.load(fs.readFileSync('./supportbot-config.yml', 'utf8'));
 
 module.exports = {
-    name: supportbot.AddUser,
+    name: supportbot.RemoveUser,
 
     execute(message, args) {        
-        console.log(`\u001b[32m`, `[${supportbot.Bot_Name}]:`, `\u001b[32m`, `${message.author.tag} has executed ${supportbot.Prefix}${supportbot.AddUser}!`);
+        console.log(`\u001b[32m`, `[${supportbot.Bot_Name}]:`, `\u001b[32m`, `${message.author.tag} has executed ${supportbot.Prefix}${supportbot.RemoveUser}!`);
 
-        let SupportStaff = message.guild.roles.cache.find(staffRole => staffRole.name === supportbot.Staff);
-
+        let staffGroup = message.guild.roles.cache.find(staffRole => staffRole.name === supportbot.Staff);
             const NoPerms = new Discord.MessageEmbed()
                 .setTitle("Invalid Permissions!")
-                .setDescription(`:warning: **Err!** You do not have the correct permissions to use this command.\n\nRole Required: \`${supportbot.SupportStaff}\``)
+                .setDescription(":warning: **Err!** You do not have the correct permissions to use this command.")
                 .setColor(supportbot.WarningColour)
             message.channel.send({ embed: NoPerms });
 
@@ -34,21 +33,21 @@ module.exports = {
         let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
             const UserNotExist = new Discord.MessageEmbed()
                 .setTitle("User Not Found!")
-                .setDescription(`:x: **Error!** This user doesn't exist, Are they in this server?\n\nTry Again:\`${supportbot.Prefix}${supportbot.AddUser} <user#0000>\``)
+                .setDescription(`:x: **Error!** This user doesn't exist, Are they in this server?\n\nTry Again:\`${supportbot.Prefix}${supportbot.RemoveUser} <user#0000>\``)
                 .setColor(supportbot.ErrorColor)
 
             if (!rUser) return message.channel.send({ embed: UserNotExist });
         
                 message.channel.updateOverwrite(rUser, {
-                    VIEW_CHANNEL: true,
+                    VIEW_CHANNEL: false,
                     CREATE_INVITE: false,
-                    SEND_MESSAGES: true,
-                    READ_MESSAGES: true
+                    SEND_MESSAGES: false,
+                    READ_MESSAGES: false
                 });
         
             const Complete = new Discord.MessageEmbed()
-                .setTitle("User Added!")
-                .setDescription(`👍 ${rUser} has been added to this ticket`)
+                .setTitle("User Removed!")
+                .setDescription(`👍 ${rUser} has been removed from this ticket`)
                 .setTimestamp()
                 .setColor(supportbot.EmbedColour)
            message.channel.send({ embed: Complete });
