f9da25643dc654058be41ee82e415b3f52a2eeea
Two <ntb.nottoday@gmail.com>
2020-07-27 10:43:30 -0400

    Update newticket.js

--- commands/newticket.js
+++ commands/newticket.js
@@ -14,6 +14,8 @@ module.exports = {
     description: supportbot.NewTicketDesc,
 
     execute(message, args) {
+      if (supportbot.DeleteMessages == "true") message.delete();
+        
       console.log(`\u001b[32m`, `[${supportbot.Bot_Name}]:`, `\u001b[32m`, `${message.author.tag} has executed ${supportbot.Prefix}${supportbot.NewTicket}!`);
 
       // Ticket ID
