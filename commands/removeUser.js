d4576dfc21a8749cc5e5819197e83fadd3987d37
Two <ntb.nottoday@gmail.com>
2020-07-27 10:43:17 -0400

    Update removeUser.js

--- commands/removeUser.js
+++ commands/removeUser.js
@@ -12,6 +12,8 @@ module.exports = {
     description: supportbot.RemoveUserDesc,
 
     execute(message, args) {        
+        if (supportbot.DeleteMessages == "true") message.delete();
+        
         console.log(`\u001b[32m`, `[${supportbot.Bot_Name}]:`, `\u001b[32m`, `${message.author.tag} has executed ${supportbot.Prefix}${supportbot.RemoveUser}!`);
 
         let SupportStaff = message.guild.roles.cache.find(SupportTeam => SupportTeam.name === supportbot.Staff)
