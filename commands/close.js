0631ab536aca1d6827ce9f3441486e3c3c374609
ItzEmerald <itz.emerald@outlook.com>
2021-06-15 19:08:46 +0100

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -15,8 +15,6 @@ module.exports = {
     execute(message, args) {
         if (supportbot.DeleteMessages) message.delete();
         
-        console.log(`\u001b[32m`, `[${supportbot.Bot_Name}]:`, `\u001b[32m`, `${message.author.tag} has executed ${supportbot.Prefix}${supportbot.CloseTicket}!`);
-
         if (!message.channel.name.startsWith( `${supportbot.TicketChannel}-` )) {
             const Exists = new Discord.MessageEmbed()
                 .setTitle("No Ticket Found!")
