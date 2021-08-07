79a498d1d6c1f8d5fdb384a2bf611d3499d3da5c
Xiled Emerald <itz.emerald@outlook.com>
2021-08-07 16:19:13 +0100

    Update transcript.js

--- commands/transcript.js
+++ commands/transcript.js
@@ -1,20 +1,16 @@
+
 // SupportBot, Created by Emerald Services
 // Announce
-
 const Discord = require("discord.js");
 const fs = require("fs");
-
 const yaml = require('js-yaml');
 const { execute } = require("./help");
 const supportbot = yaml.load(fs.readFileSync('./supportbot-config.yml', 'utf8'));
-
 module.exports = {
     name: supportbot.ChatLog,
     description: supportbot.ChatLogDesc,
-
     execute(message, args) {
         if (supportbot.DeleteMessages) message.delete();
-
         let name = message.channel.name;
     
         message.channel.messages.fetch({ limit: 100 }).then( msgs => {
@@ -38,10 +34,9 @@ module.exports = {
               .setDescription(supportbot.ChatLogSavedMessage)
               .setColor(supportbot.SuccessColour)
             message.channel.send({embed: TranscriptSavedEmbed})
-
             const errornochannel = new Discord.MessageEmbed()
                 .setTitle("SupportBot Error!")
-                .setDescription(`:x: **Error!** Channel not Found, This command cannot be executed proberbly as their is no channel within this server.\nThis is configurable via **supportbot-config.yml**\n\nChannel Required: \`${supportbot.TicketLog}\`\n\nError Code: \`SB-03\``)
+                .setDescription(`:x: **Error!** Channel not Found, This command cannot be executed proberbly as their is no channel within this server.\nThis is configurable via **supportbot-config.yml**\n\nChannel Required: \`${supportbot.ChatLogChannel}\`\n\nError Code: \`SB-03\``)
                 .setColor(supportbot.ErrorColour);
   
             const ChatLogs = new Discord.MessageEmbed()
@@ -53,7 +48,6 @@ module.exports = {
                 )
                 .setColor(supportbot.EmbedColour)
                 .setFooter(supportbot.EmbedFooter)
-
             let locateChannel = message.guild.channels.cache.find(LocateChannel => LocateChannel.name === supportbot.ChatLogChannel) || message.guild.channels.cache.find(LocateChannel => LocateChannel.id === supportbot.ChatLogChannel)
   
             if(!locateChannel) return message.channel.send({ embed: errornochannel });
