cf610fbe491ffc1031a05cc9675a5513b06534bd
Noah <noah@n0ahmc.com>
2021-10-23 20:12:04 -0500

    Ran prettier

--- Events/messageReactionAdd.js
+++ Events/messageReactionAdd.js
@@ -4,37 +4,40 @@
 const fs = require("fs");
 
 const Discord = require("discord.js");
-const yaml = require('js-yaml');
-const supportbot = yaml.load(fs.readFileSync('./Data/supportbot.yml', 'utf8'));
-const cmdconfig = yaml.load(fs.readFileSync('./Data/commands.yml', 'utf8'));
-const panelconfig = yaml.load(fs.readFileSync('./Data/ticket-panel.yml', 'utf8'));
+const yaml = require("js-yaml");
+const supportbot = yaml.load(fs.readFileSync("./Data/supportbot.yml", "utf8"));
+const cmdconfig = yaml.load(fs.readFileSync("./Data/commands.yml", "utf8"));
+const panelconfig = yaml.load(
+  fs.readFileSync("./Data/ticket-panel.yml", "utf8")
+);
 
 const Event = require("../Structures/Event.js");
 
 module.exports = new Event("messageReactionAdd", async (client) => {
+  let message = {
+    guild: reaction.message.channel.guild,
+    author: user,
+    content: "N/A",
+    channel: reaction.message.channel,
+  };
 
-    let message = {
-        guild: reaction.message.channel.guild,
-        author: user,
-        content: "N/A",
-        channel: reaction.message.channel
-    }
+  if (panelconfig.TicketPanel) {
+    if (reaction.message.id === panelconfig.TicketPanelID) {
+      if (
+        reaction.emoji.name == panelconfig.ButtonEmoji ||
+        reaction.emoji.id == panelconfig.ButtonEmoji
+      ) {
+        await reaction.users.remove(user.id);
 
-    if (panelconfig.TicketPanel) {
-        if (reaction.message.id === panelconfig.TicketPanelID) {
-            if (reaction.emoji.name == panelconfig.ButtonEmoji || reaction.emoji.id == panelconfig.ButtonEmoji) {
-                await reaction.users.remove(user.id)
+        try {
+          const cmd = client.commands.get(cmdconfig.OpenTicket);
+          if (!cmd) return;
 
-            	try {
-            	    const cmd = client.commands.get(cmdconfig.OpenTicket);
-            	    if(!cmd) return;
-            	    
-            	    cmd.run(message, message.content);
-            	} catch (error) {
-            	    console.error(error);
-            	}
-            }
+          cmd.run(message, message.content);
+        } catch (error) {
+          console.error(error);
         }
+      }
     }
-
+  }
 });
--- Structures/TicketID.js
+++ Structures/TicketID.js
@@ -8,69 +8,83 @@ const Command = require("./Command.js");
 const Client = require("./Client.js");
 const Event = require("./Event.js");
 
-const yaml = require('js-yaml');
-const supportbot = yaml.load(fs.readFileSync('./Data/supportbot.yml', 'utf8'));
+const yaml = require("js-yaml");
+const supportbot = yaml.load(fs.readFileSync("./Data/supportbot.yml", "utf8"));
 
 class TicketNumberID {
-    
-    static pad(guildId) {
-        let n = this.get(guildId) + '';
-        let z = '0';
-        let width = 4;
-        setTimeout(() => {
-            this.set(guildId, this.get(guildId) + 1);
-        }, 50);
-        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
+  static pad(guildId) {
+    let n = this.get(guildId) + "";
+    let z = "0";
+    let width = 4;
+    setTimeout(() => {
+      this.set(guildId, this.get(guildId) + 1);
+    }, 50);
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  }
 
-static get(guildId) {
-
-    let ticketnumberID = JSON.parse(fs.readFileSync("./Data/TicketData.json", "utf8"));
+  static get(guildId) {
+    let ticketnumberID = JSON.parse(
+      fs.readFileSync("./Data/TicketData.json", "utf8")
+    );
 
     if (!ticketnumberID[guildId]) {
-        ticketnumberID[guildId] = {
-            id: 0
+      ticketnumberID[guildId] = {
+        id: 0,
+      };
+
+      fs.writeFileSync(
+        "./Data/TicketData.json",
+        JSON.stringify(ticketnumberID, null, 4),
+        (err) => {
+          if (err) console.log(err);
         }
+      );
 
-        fs.writeFileSync("./Data/TicketData.json", JSON.stringify(ticketnumberID, null, 4), err => {
-            if (err) console.log(err);
-        });
-
-        return 0;
-
-        
+      return 0;
     } else {
-        return ticketnumberID[guildId].id;
+      return ticketnumberID[guildId].id;
     }
-}
+  }
 
-static set(guildId, value) {
-    let ticketnumberID = JSON.parse(fs.readFileSync("./Data/TicketData.json", "utf8"));
-    
+  static set(guildId, value) {
+    let ticketnumberID = JSON.parse(
+      fs.readFileSync("./Data/TicketData.json", "utf8")
+    );
+
     ticketnumberID[guildId] = {
-        id: value
-    }
+      id: value,
+    };
 
-    fs.writeFileSync("./Data/TicketData.json", JSON.stringify(ticketnumberID, null, 4), err => {
+    fs.writeFileSync(
+      "./Data/TicketData.json",
+      JSON.stringify(ticketnumberID, null, 4),
+      (err) => {
         if (err) console.log(err);
-    });
+      }
+    );
 
     return value;
-}
+  }
 
-static reset(guildId) {
-    let ticketnumberID = JSON.parse(fs.readFileSync("./Data/TicketData.json", "utf8"));
-    
+  static reset(guildId) {
+    let ticketnumberID = JSON.parse(
+      fs.readFileSync("./Data/TicketData.json", "utf8")
+    );
+
     ticketnumberID[guildId] = {
-        id: 0
-    }
+      id: 0,
+    };
 
-    fs.writeFileSync("./Data/TicketData.json", JSON.stringify(ticketnumberID, null, 4), err => {
+    fs.writeFileSync(
+      "./Data/TicketData.json",
+      JSON.stringify(ticketnumberID, null, 4),
+      (err) => {
         if (err) console.log(err);
-    });
+      }
+    );
 
     return 0;
-}
+  }
 }
 
 module.exports = TicketNumberID;
