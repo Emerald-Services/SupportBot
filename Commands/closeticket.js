33e4d97be5da1a478ce9438934722fda555e5c4f
Noah <noah@n0ahmc.com>
2021-10-23 20:04:02 -0500

    Updated for slash commands

--- Commands/closeticket.js
+++ Commands/closeticket.js
@@ -4,171 +4,224 @@
 const fs = require("fs");
 
 const Discord = require("discord.js");
-const yaml = require('js-yaml');
-const supportbot = yaml.load(fs.readFileSync('./Data/supportbot.yml', 'utf8'));
-const cmdconfig = yaml.load(fs.readFileSync('./Data/commands.yml', 'utf8'));
+const yaml = require("js-yaml");
+const supportbot = yaml.load(fs.readFileSync("./Data/supportbot.yml", "utf8"));
+const cmdconfig = yaml.load(fs.readFileSync("./Data/commands.yml", "utf8"));
 
 const Command = require("../Structures/Command.js");
-const TicketNumberID = require('../Structures/TicketID.js');
+const TicketNumberID = require("../Structures/TicketID.js");
 
 async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-    }
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
   }
+}
 
 module.exports = new Command({
-	name: cmdconfig.CloseTicket,
-	description: cmdconfig.CloseTicketDesc,
-	type: "BOTH",
-	slashCommandOptions: [],
-	permission: "SEND_MESSAGES",
+  name: cmdconfig.CloseTicket,
+  description: cmdconfig.CloseTicketDesc,
+  slashCommandOptions: [
+    {
+      name: "reason",
+      description: "Ticket Close Reason",
+      type: "STRING",
+    },
+  ],
+  permission: "SEND_MESSAGES",
 
-	async run(message, args, client) {
-        
-        if (!message.channel.name.startsWith( `${supportbot.TicketChannel}-` )) {
-            const Exists = new Discord.MessageEmbed()
-                .setTitle("No Ticket Found!")
-                .setDescription(`${supportbot.NoValidTicket}`)
-                .setColor(supportbot.WarningColour);
-            message.channel.send({ embed: Exists });
+  async run(interaction) {
+    if (!interaction.channel.name.startsWith(`${supportbot.TicketChannel}-`)) {
+      const Exists = new Discord.MessageEmbed()
+        .setTitle("No Ticket Found!")
+        .setDescription(`${supportbot.NoValidTicket}`)
+        .setColor(supportbot.WarningColour);
+      return interaction.reply({ embed: Exists });
+    }
 
-            return;
+    if (supportbot.CloseConfirmation) {
+      const CloseTicketRequest = new Discord.MessageEmbed()
+        .setTitle(`**${supportbot.ClosingTicket}**`)
+        .setDescription(
+          `Please confirm by repeating the following word.. \`${supportbot.ClosingConfirmation_Word}\` `
+        )
+        .setColor(supportbot.EmbedColour);
 
-        }
+      interaction
+        .reply(CloseTicketRequest)
+        .then((m) => {
+          interaction.channel
+            .awaitMessages(
+              (response) =>
+                response.content.toLowerCase() ===
+                supportbot.ClosingConfirmation_Word,
+              {
+                max: 1,
+                time: 20000,
+                errors: ["time"],
+              }
+            )
+            .then((collected) => {
+              let logChannel =
+                interaction.guild.channels.cache.find(
+                  (channel) => channel.name === supportbot.TranscriptLog
+                ) ||
+                interaction.guild.channels.cache.find(
+                  (channel) => channel.id === supportbot.TranscriptLog
+                );
+              let user = interaction.user;
+              let reason =
+                interaction.options.getString("reason") ||
+                "No Reason Provided.";
 
-        if (supportbot.CloseConfirmation) {
-            const CloseTicketRequest = new Discord.MessageEmbed()
-                .setTitle(`**${supportbot.ClosingTicket}**`)
-                .setDescription(`Please confirm by repeating the following word.. \`${supportbot.ClosingConfirmation_Word}\` `)
-                .setColor(supportbot.EmbedColour);
+              let name = interaction.channel.name;
+              let ticketChannel = interaction.channel;
 
-            message.channel.send(CloseTicketRequest).then(m => {
-                message.channel.awaitMessages( response => response.content.toLowerCase() === supportbot.ClosingConfirmation_Word, {
-                    max: 1,
-                    time: 20000,
-                    errors: [ 'time' ],
+              interaction
+                .reply(`**${supportbot.ClosingTicket}**`)
 
-                    
-                }).then( collected => {
-                    let logChannel = message.guild.channels.cache.find(channel => channel.name === supportbot.TranscriptLog) || message.guild.channels.cache.find(channel => channel.id === supportbot.TranscriptLog)
-                    let user = message.author;
-                    let reason = args.join(" ") || "No Reason Provided.";
+                .then(() => {
+                  const logEmbed = new Discord.MessageEmbed()
+                    .setTitle(supportbot.TranscriptTitle)
+                    .setColor(supportbot.EmbedColour)
+                    .setFooter(supportbot.EmbedFooter)
+                    .addField("Closed By", interaction.user.tag)
+                    .addField("Reason", reason);
 
-                    let name = message.channel.name;
-                    let ticketChannel = message.channel;
+                  interaction.channel.interactions
+                    .fetch({ limit: 100 })
+                    .then((msgs) => {
+                      let html = "";
 
-                    message.channel.send(`**${supportbot.ClosingTicket}**`)
+                      msgs = msgs.sort(
+                        (a, b) => a.createdTimestamp - b.createdTimestamp
+                      );
+                      html += `<strong>Server Name:</strong> ${interaction.guild.name}<br>`;
+                      html += `<strong>Ticket:</strong> ${ticketChannel.name}<br>`;
+                      html += `<strong>Message:</strong> ${msgs.size} Messages<br><br><br>`;
 
-                    .then(() => {
-                        const logEmbed = new Discord.MessageEmbed()
-                            .setTitle(supportbot.TranscriptTitle)
-                            .setColor(supportbot.EmbedColour)
-                            .setFooter(supportbot.EmbedFooter)
-                            .addField("Closed By", message.author.tag)
-                            .addField("Reason", reason);
+                      msgs.forEach((msg) => {
+                        if (msg.content) {
+                          html += `<strong>User:</strong> ${msg.author.tag}<br>`;
+                          html += `<strong>Message:</strong> ${msg.content}<br>`;
+                          html += `-----<br><br>`;
+                        }
+                      });
 
-                        message.channel.messages.fetch({ limit: 100 }).then( msgs => {
-                            let html = '';
+                      logChannel.send({ embeds: [logEmbed] }).catch((err) => {
+                        interaction.reply(err);
+                      });
 
-                            msgs = msgs.sort( ( a, b ) => a.createdTimestamp - b.createdTimestamp );
-                            html += `<strong>Server Name:</strong> ${message.guild.name}<br>`;
-                            html += `<strong>Ticket:</strong> ${ticketChannel.name}<br>`;
-                            html += `<strong>Message:</strong> ${msgs.size} Messages<br><br><br>`;
+                      let file = new Discord.MessageAttachment(
+                        Buffer.from(html),
+                        `${name}.html`
+                      );
+                      logChannel
+                        .send({ embeds: [logEmbed], files: [file] })
+                        .catch((err) => {
+                          interaction.reply(err);
+                        });
 
-                            msgs.forEach( msg => {
-                                if ( msg.content ) {
-                                    html += `<strong>User:</strong> ${msg.author.tag}<br>`;
-                                    html += `<strong>Message:</strong> ${msg.content}<br>`;
-                                    html += `-----<br><br>`;
-                                }
-                            });
-
-                            logChannel.send({embeds: [logEmbed]}).catch(err => {
-                                message.reply(err)
-                            })
-
-
-                            let file = new Discord.MessageAttachment( Buffer.from( html ), `${name}.html` )
-                            logChannel.send({embeds: [logEmbed], files: [file]}).catch(err => {
-                                message.reply(err)
-                            })
-
-                            message.channel.delete().catch(error => {
-                                if (error.code !== Discord.Constants.APIErrors.UNKNOWN_CHANNEL) {
-                                    console.error('Failed to delete the message:', error);
-                                }
-                            });
-
-
-                        })
-                    })
-
-                })
-
-            }).catch( () => {
-                interaction.m.edit({content: 'The request to close the ticket has timed out.'}).then(m2 => setTimeout(() => channel.delete(3000)) );
-            });
-
-        }
-
-        if (!supportbot.CloseConfirmation) {
-
-                    let logChannel = message.guild.channels.cache.find(channel => channel.name === supportbot.TranscriptLog) || message.guild.channels.cache.find(channel => channel.id === supportbot.TranscriptLog)
-                    let user = message.author;
-                    let reason = args.join(" ") || "No Reason Provided.";
-
-                    let name = message.channel.name;
-                    let ticketChannel = message.channel;
-
-                    message.channel.send(`**${supportbot.ClosingTicket}**`)
-
-                    .then(() => {
-                        const logEmbed = new Discord.MessageEmbed()
-                            .setTitle(supportbot.TranscriptTitle)
-                            .setColor(supportbot.EmbedColour)
-                            .setFooter(supportbot.EmbedFooter)
-                            .addField("Closed By", message.author.tag)
-                            .addField("Reason", reason);
-
-                        message.channel.messages.fetch({ limit: 100 }).then( msgs => {
-                            let html = '';
-
-                            msgs = msgs.sort( ( a, b ) => a.createdTimestamp - b.createdTimestamp );
-                            html += `<strong>Server Name:</strong> ${message.guild.name}<br>`;
-                            html += `<strong>Ticket:</strong> ${ticketChannel.name}<br>`;
-                            html += `<strong>Message:</strong> ${msgs.size} Messages<br><br><br>`;
-
-                            msgs.forEach( msg => {
-                                if ( msg.content ) {
-                                    html += `<strong>User:</strong> ${msg.author.tag}<br>`;
-                                    html += `<strong>Message:</strong> ${msg.content}<br>`;
-                                    html += `-----<br><br>`;
-                                }
-                            });
-
-                            logChannel.send({embeds: [logEmbed]}).catch(err => {
-                                message.reply(err)
-                            })
-
-                            let file = new Discord.MessageAttachment( Buffer.from( html ), `${name}.html` )
-                            logChannel.send({embeds: [logEmbed], files: [file]}).catch(err => {
-                                message.reply(err)
-                            })
-
-                            message.channel.delete().catch(error => {
-                                if (error.code !== Discord.Constants.APIErrors.UNKNOWN_CHANNEL) {
-                                    console.error('Failed to delete the message:', error);
-                                }
-                            });
-
-                        })
-
-                    }).catch( () => {
-                        interaction.m.edit({content: 'The request to close the ticket has timed out.'}).then(m2 => setTimeout(() => channel.delete(3000)) );
+                      interaction.channel.delete().catch((error) => {
+                        if (
+                          error.code !==
+                          Discord.Constants.APIErrors.UNKNOWN_CHANNEL
+                        ) {
+                          console.error(
+                            "Failed to delete the interaction:",
+                            error
+                          );
+                        }
+                      });
                     });
+                });
+            });
+        })
+        .catch(() => {
+          interaction
+            .followUp({
+              content: "The request to close the ticket has timed out.",
+            })
+            .then((m2) => setTimeout(() => interaction.channel.delete(3000)));
+        });
+    }
 
-        }
+    if (!supportbot.CloseConfirmation) {
+      let logChannel =
+        interaction.guild.channels.cache.find(
+          (channel) => channel.name === supportbot.TranscriptLog
+        ) ||
+        interaction.guild.channels.cache.find(
+          (channel) => channel.id === supportbot.TranscriptLog
+        );
+      let user = interaction.user;
+      let reason =
+        (await interaction.options.getString("reason")) ||
+        "No Reason Provided.";
 
-	}
+      let name = interaction.channel.name;
+      let ticketChannel = interaction.channel;
+
+      interaction
+        .reply(`**${supportbot.ClosingTicket}**`)
+
+        .then(() => {
+          const logEmbed = new Discord.MessageEmbed()
+            .setTitle(supportbot.TranscriptTitle)
+            .setColor(supportbot.EmbedColour)
+            .setFooter(supportbot.EmbedFooter)
+            .addField("Closed By", interaction.user.tag)
+            .addField("Reason", reason);
+
+          interaction.channel.interactions
+            .fetch({ limit: 100 })
+            .then((msgs) => {
+              let html = "";
+
+              msgs = msgs.sort(
+                (a, b) => a.createdTimestamp - b.createdTimestamp
+              );
+              html += `<strong>Server Name:</strong> ${interaction.guild.name}<br>`;
+              html += `<strong>Ticket:</strong> ${ticketChannel.name}<br>`;
+              html += `<strong>Message:</strong> ${msgs.size} Messages<br><br><br>`;
+
+              msgs.forEach((msg) => {
+                if (msg.content) {
+                  html += `<strong>User:</strong> ${msg.author.tag}<br>`;
+                  html += `<strong>Message:</strong> ${msg.content}<br>`;
+                  html += `-----<br><br>`;
+                }
+              });
+
+              logChannel.send({ embeds: [logEmbed] }).catch((err) => {
+                interaction.reply(err);
+              });
+
+              let file = new Discord.MessageAttachment(
+                Buffer.from(html),
+                `${name}.html`
+              );
+              logChannel
+                .send({ embeds: [logEmbed], files: [file] })
+                .catch((err) => {
+                  interaction.reply(err);
+                });
+
+              interaction.channel.delete().catch((error) => {
+                if (
+                  error.code !== Discord.Constants.APIErrors.UNKNOWN_CHANNEL
+                ) {
+                  console.error("Failed to delete the interaction:", error);
+                }
+              });
+            });
+        })
+        .catch(() => {
+          interaction
+            .followUp({
+              content: "The request to close the ticket has timed out.",
+            })
+            .then((m2) => setTimeout(() => interaction.channel.delete(3000)));
+        });
+    }
+  },
 });
--- Commands/help.js
+++ Commands/help.js
@@ -4,83 +4,102 @@
 const fs = require("fs");
 
 const Discord = require("discord.js");
-const yaml = require('js-yaml');
-const supportbot = yaml.load(fs.readFileSync('./Data/supportbot.yml', 'utf8'));
-const cmdconfig = yaml.load(fs.readFileSync('./Data/commands.yml', 'utf8'));
+const yaml = require("js-yaml");
+const supportbot = yaml.load(fs.readFileSync("./Data/supportbot.yml", "utf8"));
+const cmdconfig = yaml.load(fs.readFileSync("./Data/commands.yml", "utf8"));
 
 const Command = require("../Structures/Command.js");
-const TicketNumberID = require('../Structures/TicketID.js');
+const TicketNumberID = require("../Structures/TicketID.js");
 
 async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-    }
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
   }
+}
 
 module.exports = new Command({
-	name: cmdconfig.HelpCommand,
-	description: cmdconfig.HelpCommandDesc,
-	type: "BOTH",
-	slashCommandOptions: [],
-	permission: "SEND_MESSAGES",
+  name: cmdconfig.HelpCommand,
+  description: cmdconfig.HelpCommandDesc,
+  slashCommandOptions: [],
+  permission: "SEND_MESSAGES",
 
-	async run(message, args, client) {
+  async run(interaction) {
+    let SupportStaff =
+      interaction.guild.roles.cache.find(
+        (adminRole) => adminRole.name === supportbot.Staff
+      ) ||
+      interaction.guild.roles.cache.find(
+        (staffRole) => staffRole.id === supportbot.Staff
+      );
+    let AdminStaff =
+      interaction.guild.roles.cache.find(
+        (adminRole) => adminRole.name === supportbot.Admin
+      ) ||
+      interaction.guild.roles.cache.find(
+        (adminRole) => adminRole.id === supportbot.Admin
+      );
 
-        let SupportStaff = message.guild.roles.cache.find(adminRole => adminRole.name === supportbot.Staff) || message.guild.roles.cache.find(staffRole => staffRole.id === supportbot.Staff);
-        let AdminStaff = message.guild.roles.cache.find(adminRole => adminRole.name === supportbot.Admin) || message.guild.roles.cache.find(adminRole => adminRole.id === supportbot.Admin);
-            
-        if (supportbot.HelpMenu) {
-            let botCommands = "";
-                botCommands += `**${supportbot.Prefix}${cmdconfig.HelpCommand}** ${cmdconfig.HelpCommandDesc}\n`;
-                botCommands += `**${supportbot.Prefix}${cmdconfig.InfoCommand}** ${cmdconfig.InfoCommandDesc}\n`;
-                botCommands += `**${supportbot.Prefix}${cmdconfig.PingCommand}** ${cmdconfig.PingCommandDesc}\n`
-            
-            let ticketCommands = "";
-                ticketCommands += `**${supportbot.Prefix}${cmdconfig.OpenTicket}** ${cmdconfig.OpenTicketDesc}\n`;
-                ticketCommands += `**${supportbot.Prefix}${cmdconfig.CloseTicket}** ${cmdconfig.CloseTicketDesc}\n`
+    if (supportbot.HelpMenu) {
+      let botCommands = "";
+      botCommands += `**${supportbot.Prefix}${cmdconfig.HelpCommand}** ${cmdconfig.HelpCommandDesc}\n`;
+      botCommands += `**${supportbot.Prefix}${cmdconfig.InfoCommand}** ${cmdconfig.InfoCommandDesc}\n`;
+      botCommands += `**${supportbot.Prefix}${cmdconfig.PingCommand}** ${cmdconfig.PingCommandDesc}\n`;
 
-            let staffCommands = "";
-                staffCommands += `**${supportbot.Prefix}${cmdconfig.TicketAdd}** ${cmdconfig.TicketAddDesc}\n`
-                staffCommands += `**${supportbot.Prefix}${cmdconfig.TicketRemove}** ${cmdconfig.TicketRemoveDesc}\n`
-                
-            const HelpEmbed1 = new Discord.MessageEmbed()
-                .setTitle(supportbot.Name)
-                .setThumbnail(message.author.displayAvatarURL())
-    
-                .addFields(
-                    { name: '🖥️ General Commands\n', value: `${botCommands}\n`, inline: false },
-                    { name: '🎫 Support Commands\n', value: `${ticketCommands}\n`, inline: false },
-                )
-    
-                .setColor(supportbot.EmbedColour)
-                .setFooter(supportbot.EmbedFooter, message.author.displayAvatarURL());
-    
-                 if (message.member.roles.cache.has(SupportStaff.id)) {
-                    HelpEmbed1.addFields(
-                        { name: '🔐 Staff Commands\n', value: `${staffCommands}\n`, inline: false },
-                    )
-                } else {
-                    if ((message.member.roles.cache.has(AdminStaff.id))) {
-                        HelpEmbed1.addFields(
-                            { name: '🔐 Staff Commands\n', value: `${staffCommands}\n`, inline: false },
-                        )
-                    }
-                }
-    
-            if (supportbot.SendHelpPage === "dm") {
-                message.author.send({ 
-                    embeds: [HelpEmbed1]
-                });
-            }
-    
-            if (supportbot.SendHelpPage === "channel") {
-                message.channel.send({ 
-                    embeds: [HelpEmbed1]
-                });
-            }
-    
-    
+      let ticketCommands = "";
+      ticketCommands += `**${supportbot.Prefix}${cmdconfig.OpenTicket}** ${cmdconfig.OpenTicketDesc}\n`;
+      ticketCommands += `**${supportbot.Prefix}${cmdconfig.CloseTicket}** ${cmdconfig.CloseTicketDesc}\n`;
+
+      let staffCommands = "";
+      staffCommands += `**${supportbot.Prefix}${cmdconfig.TicketAdd}** ${cmdconfig.TicketAddDesc}\n`;
+      staffCommands += `**${supportbot.Prefix}${cmdconfig.TicketRemove}** ${cmdconfig.TicketRemoveDesc}\n`;
+
+      const HelpEmbed1 = new Discord.MessageEmbed()
+        .setTitle(supportbot.Name + " Commands")
+        .setThumbnail(interaction.user.displayAvatarURL())
+
+        .addFields(
+          {
+            name: "🖥️ General Commands\n",
+            value: `${botCommands}\n`,
+            inline: false,
+          },
+          {
+            name: "🎫 Support Commands\n",
+            value: `${ticketCommands}\n`,
+            inline: false,
+          }
+        )
+
+        .setColor(supportbot.EmbedColour)
+        .setFooter(supportbot.EmbedFooter, interaction.user.displayAvatarURL());
+
+      if (interaction.member.roles.cache.has(SupportStaff.id)) {
+        HelpEmbed1.addFields({
+          name: "🔐 Staff Commands\n",
+          value: `${staffCommands}\n`,
+          inline: false,
+        });
+      } else {
+        if (interaction.member.roles.cache.has(AdminStaff.id)) {
+          HelpEmbed1.addFields({
+            name: "🔐 Staff Commands\n",
+            value: `${staffCommands}\n`,
+            inline: false,
+          });
         }
-    
-        }
+      }
+
+      if (supportbot.SendHelpPage === "dm") {
+        interaction.user.reply({
+          embeds: [HelpEmbed1],
+        });
+      }
+
+      if (supportbot.SendHelpPage === "channel") {
+        interaction.reply({
+          embeds: [HelpEmbed1],
+        });
+      }
+    }
+  },
 });
