02394e310d6009a468fa9bf1c5972f2ced907142
Noah <noah@n0ahmc.com>
2021-11-17 19:04:38 -0600

    Exit if joins or leaves guild

---
+++ Events/guildCreate.js
@@ -0,0 +1,9 @@
+const Event = require("../Structures/Event.js");
+module.exports = new Event("guildCreate", async (client) => {
+  console.log(
+    `\u001b[31m`,
+    `${client.user.username} is not in the correct server set in your config. Please leave all other servers and restart the bot.`
+  );
+  console.log(`\u001b[31m`, `${client.user.username} will now exit.`);
+  return process.exit(1);
+});
