e83ca8cb2b5882c333957eb49a92267ff41cb074
Noah <noah@n0ahmc.com>
2021-12-18 18:58:02 -0600

    add guild

--- Events/guildDelete.js
+++ Events/guildDelete.js
@@ -1,6 +1,6 @@
 const Event = require("../Structures/Event.js");
 
-module.exports = new Event("guildDelete", async (client) => {
+module.exports = new Event("guildDelete", async (client, guild) => {
   console.log(
     `\u001b[31m`,
     `${client.user.username} is not in the correct server set in your config. Please join the server and restart the bot.`
