4d7583785ba175d80b7e7ee79256f43f09312b11
Noah <noah@n0ahmc.com>
2021-12-18 22:14:11 -0600

    Fixed ticket id

--- Structures/TicketID.js
+++ Structures/TicketID.js
@@ -5,44 +5,31 @@ const fs = require("fs");
 
 class TicketNumberID {
   static async pad() {
-    let n = this.get() + "";
-    let z = "0";
-    let width = 4;
-    await this.set(this.get() + 1);
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    let num = await this.get();
+    let n = num + "";
+    await this.set(num + 1);
+    return n.length >= 4 ? n : new Array(4 - n.length + 1).join("0") + n;
   }
 
-  static get() {
-    let ticketnumberID = JSON.parse(
-      fs.readFileSync("./Data/TicketData.json", "utf8")
+  static async get() {
+    let ticketnumberID = await JSON.parse(
+      fs.readFileSync("./Data/ticket-panel-id.json", "utf8")
     );
-
     if (!ticketnumberID.id) {
-      ticketnumberID.id = 0;
-
-      fs.writeFileSync(
-        "./Data/TicketData.json",
-        JSON.stringify(ticketnumberID, null, 4),
-        (err) => {
-          if (err) console.error(err);
-        }
-      );
-
-      return 0;
+      return this.set(0);
     } else {
       return ticketnumberID.id;
     }
   }
 
-  static set(value) {
-    let ticketnumberID = JSON.parse(
-      fs.readFileSync("./Data/TicketData.json", "utf8")
+  static async set(value) {
+    let ticketnumberID = await JSON.parse(
+      fs.readFileSync("./Data/ticket-panel-id.json", "utf8")
     );
 
     ticketnumberID.id = value;
-
     fs.writeFileSync(
-      "./Data/TicketData.json",
+      "./Data/ticket-panel-id.json",
       JSON.stringify(ticketnumberID, null, 4),
       (err) => {
         if (err) console.error(err);
@@ -52,22 +39,8 @@ class TicketNumberID {
     return value;
   }
 
-  static reset() {
-    let ticketnumberID = JSON.parse(
-      fs.readFileSync("./Data/TicketData.json", "utf8")
-    );
-
-    ticketnumberID.id = 0;
-
-    fs.writeFileSync(
-      "./Data/TicketData.json",
-      JSON.stringify(ticketnumberID, null, 4),
-      (err) => {
-        if (err) console.error(err);
-      }
-    );
-
-    return 0;
+  static async reset() {
+    return await this.set(0);
   }
 }
 
