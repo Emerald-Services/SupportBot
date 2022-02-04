23acc0926f1ddbf22e0577baedebf0460a467730
Noah <noah@n0ahmc.com>
2022-02-04 17:49:02 -0600

    Fix BitField Error

--- Structures/Command.js
+++ Structures/Command.js
@@ -5,7 +5,7 @@ class Command {
   constructor(options) {
     this.name = options.name;
     this.description = options.description;
-    this.permissions = options.permissions;
+    this.permissions = options.permissions || [];
     this.options = options.options || [];
     this.run = options.run;
     this.defaultPermission = true;
